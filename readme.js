eb906cb41c7f3fbf234f56bdf9e744ea49294c89
jayamiko <jayamiko4@gmail.com>
2023-01-29 13:17:39 +0700

    first commit project

---
+++ readme.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createStackNavigator} from '@react-navigation/stack';
+
+// import HomePage from './src/screen/homePage/homepage';
+// import SplashScreen from './src/screen/splashScreen/SplashScreen';
+
+// const Stack = createStackNavigator();
+
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator
+//         initialRouteName="SplashScreen"
+//         screenOptions={{headerShown: false}}>
+//         <Stack.Screen name="Home" component={HomePage} />
+//         <Stack.Screen name="SplashScreen" component={SplashScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+// export default App;
